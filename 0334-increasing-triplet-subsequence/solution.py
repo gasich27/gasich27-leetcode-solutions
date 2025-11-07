ab7e7576a8bd3a35eb5075baba2b2dd902861ec5
Gasich <139581464+gasich27@users.noreply.github.com>
2025-11-07 18:47:04 +0000

    [LeetCode] Runtime - 27 ms (40.17%), Memory - 24.4 MB (26.63%)

---
+++ 0334-increasing-triplet-subsequence/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def increasingTriplet(self, nums):
+        first = second = float('inf')
+    
+        for i in nums:
+            if i <= first:
+                first = i
+            elif i <= second:
+                second = i
+            else:
+                return True
+        return False
+        
