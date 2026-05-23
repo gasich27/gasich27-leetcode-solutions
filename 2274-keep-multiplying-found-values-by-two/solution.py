0e03c83f5a7cd66b1a9655a9693211fd8d94fe5b
Gasich <139581464+gasich27@users.noreply.github.com>
2026-05-23 18:36:04 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 19.4 MB (34.55%)

---
+++ 2274-keep-multiplying-found-values-by-two/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        n = nums.sort()
+
+        for i in range(len(nums)):
+            if nums[i] == original:
+                original = original * 2
+
+        return original
