20b5a6a6285cc3080e2ac461ddb055ce58550912
Gasich <139581464+gasich27@users.noreply.github.com>
2026-08-10 10:46:31 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 20.2 MB (63.46%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,9 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
+        dicst = {}
+        n = len(nums)
 
-        for i, x in enumerate(nums):
-            need = target - x
-            if need in seen:
-                return [seen[need], i]
-            seen[x] = i
+        for i in range(n):
+            comp = target - nums[i]
+            if comp in dicst:
+                return [dicst[comp], i]
+            else:
+                dicst[nums[i]] = i
+
+
+        
