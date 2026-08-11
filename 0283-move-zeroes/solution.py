49a44e5b046f37dacfc7864b3c5939bd28029011
Gasich <139581464+gasich27@users.noreply.github.com>
2026-08-11 09:42:52 +0000

    [LeetCode] Runtime - 7 ms (39.16%), Memory - 20.5 MB (62.04%)

--- 0283-move-zeroes/solution.py
+++ 0283-move-zeroes/solution.py
@@ -1,13 +1,7 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        c = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[c] = nums[i]
-                c += 1
-    
-        for j in range(c, len(nums)):
-            nums[j] = 0
-    
-        return nums
-        
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1        
